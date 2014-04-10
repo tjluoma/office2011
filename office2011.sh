32d12c0cdcd37b550ff308278c2daf607a0813fb
TJ Luoma <github@tjluoma.com>
2014-04-09 23:55:11 -0400

    Updated for 14.4.1 Update
    Microsoft Office for Mac 2011 14.4.1 Update

--- office2011.sh
+++ office2011.sh
@@ -232,7 +232,7 @@ fi
 for DMG in 	X18-08827.dmg \
 			MERP_229.dmg \
 			AutoUpdate_236.dmg \
-			Office2011-1439Update_EN-US.dmg
+			Office2011-1441Update_EN-US.dmg
 do
 
 MIN_VERSION=''
@@ -326,13 +326,13 @@ case "${DMG}" in
 	;;
 
 
-	Office2011-1439Update_EN-US.dmg)
-		FULL_NAME='Microsoft Office for Mac 2011 14.3.9 Update'
-		MORE_INFO='http://www.microsoft.com/en-us/download/details.aspx?id=41155'
-		RECEIPT='com.microsoft.office.all.core.pkg.14.3.9.update'
-		DL_URL='http://download.microsoft.com/download/E/C/6/EC6221F3-463C-46FF-8365-8784A67D7872/Office2011-1439Update_EN-US.dmg'
-		BYTES='118839106'
-		SUM='9e10855139ae39de7118485e2bf45b93ad0a82e67d351040bd972ce603040129'
+	Office2011-1441Update_EN-US.dmg)
+		FULL_NAME='Microsoft Office for Mac 2011 14.4.1 Update'
+		MORE_INFO='http://www.microsoft.com/en-us/download/details.aspx?id=42373'
+		RECEIPT='com.microsoft.office.all.core.pkg.14.4.1.update'
+		DL_URL='http://download.microsoft.com/download/A/6/9/A69FE062-D726-456E-A8AA-B1D2A292300E/Office2011-1441Update_EN-US.dmg'
+		BYTES='118984586'
+		SUM='340ff8b731c89f103927db4a2571fa81c9fcb0dc683f394919fda12bfc793af7'
 		MIN_VERSION='14.1.0'
 	;;
 
